071238de17c5cacfd8f0368e964d1b8391de27e4
William Santos <110426773+willsantos86@users.noreply.github.com>
2022-09-03 21:32:25 -0300

    Aula sobre funções de Matemática.

---
+++ ex_aula_funçõesMatheRandom.py
@@ -0,0 +1,32 @@
+import math
+
+#print(abs(500))
+#print(abs(-500))
+#print(math.ceil(5.5632))
+#print(math.floor(5.5632))
+#print(math.pow(7,8))
+
+import random
+
+#string = 'abacate laranja tomate'
+#print(random.choice(string))
+#lista = ['abacate','laranja', 'tomate']
+#print(random.choice(lista))
+#tupla = ('abacate', 'laranja', 'tomate')
+#print(random.choice(tupla))
+/                      o
+print(random.randrange(1,10))
+print(random.choice(range(1,10)))
+
+nomes = ['Alice', 'Bob', 'Carl', 'Daniele', 'Edgard']
+for n in range(5):
+    random.shuffle(nomes)
+    print(f'Na passagem de número {n +1} a lista estava assim: {nomes}')
+
+nomes = ['Alice', 'Bob', 'Carl', 'Daniele', 'Edgard']
+for n in range(5):
+    nomes: random.sample(nomes, k = len(nomes))
+    print(f'Na passagem de número {n + 1} a tupla estava assim:{nomes}')
+
+tupla = (1, 2, 3, 4)
+print(tupla)
