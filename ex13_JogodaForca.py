0c7e411eacbc1487ad7d16e1e3823c8b6d069933
William Santos <110426773+willsantos86@users.noreply.github.com>
2022-09-14 09:34:21 -0300

    Atualização do Jogo da forca.

--- ex13_JogodaForca.py
+++ ex13_JogodaForca.py
@@ -1,11 +1,49 @@
-usuario = input('Digite uma palavra: ')
-print(f'Essa palavra tem {len(usuario)} letras!')
+#%%
+tentativa_ok = ''
+tentativa_nok = ''
+palavra = input('Digite uma palavra: ')
+print(f'Essa palavra tem {len(palavra)} letras!')
 
-cont = 0
-while cont <= 6:
+
+while True:
     tentativa = input('Digite uma letra: ')
-    cont += 1
-    for l in usuario:
+
+    for l in palavra:
         if l == tentativa:
-            print('Acertou uma letra!')
+            tentativa_ok += tentativa
+            print('Acertou!')
+        elif l != tentativa:
+            tentativa_nok += tentativa
+            print(f'Errou! A letra {tentativa} não contém na palavra secreta!')
+    if len(tentativa_nok) >= 6:
+        print(f'Fim de jogo!! Você esgotou seu número de tentativas!')
+        break
+    if tentativa_ok == palavra:
+        print(f'Parabéns, você venceu!! A palavra é {palavra}')
+        break
+
+
         
+# %%
+tentativa_ok = ''
+tentativa_nok = ''
+palavra = input('Digite uma palavra: ')
+print(f'Essa palavra tem {len(palavra)} letras!')
+
+
+while True:
+    tentativa = input('Digite uma letra: ')
+   
+    if tentativa in palavra:
+        tentativa_ok += tentativa
+        print('Acertou!')
+    elif tentativa not in palavra:
+        tentativa_nok += tentativa
+        print(f'Errou! A letra {tentativa} não contém na palavra secreta!')
+    if len(tentativa_nok) >= 6:
+        print(f'Fim de jogo!! Você esgotou seu número de tentativas!')
+        break
+    if tentativa_ok == palavra:
+        print(f'Parabéns, você venceu!! A palavra é {palavra}')
+        break
+# %%
