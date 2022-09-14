d941c72ed001ac63beeed1c382ae36433aa25993
William Santos <110426773+willsantos86@users.noreply.github.com>
2022-09-14 15:07:56 -0300

    Aula  de funções temporais.

--- ex13_JogodaForca.py
+++ ex13_JogodaForca.py
@@ -1,39 +1,24 @@
-#%%
-tentativa_ok = ''
-tentativa_nok = ''
-palavra = input('Digite uma palavra: ')
-print(f'Essa palavra tem {len(palavra)} letras!')
+"""Programa de 'Jogo de Forca'. O usuário deverá digitar uma palavra,
+ que será a resposta, em seguida, informe ao usuário o tamanho da palavra
+ e peça para adivinhar a primeira letra,a segunda etc. até descobrir a 
+ palavra ou errar 6 vezes. O jogo acaba quando uma das condições 
+ for satisfeita."""     
 
-
-while True:
-    tentativa = input('Digite uma letra: ')
-
-    for l in palavra:
-        if l == tentativa:
-            tentativa_ok += tentativa
-            print('Acertou!')
-        elif l != tentativa:
-            tentativa_nok += tentativa
-            print(f'Errou! A letra {tentativa} não contém na palavra secreta!')
-    if len(tentativa_nok) >= 6:
-        print(f'Fim de jogo!! Você esgotou seu número de tentativas!')
-        break
-    if tentativa_ok == palavra:
-        print(f'Parabéns, você venceu!! A palavra é {palavra}')
-        break
-
-
-        
 # %%
 tentativa_ok = ''
 tentativa_nok = ''
+separador = '-'
+cont = 1
 palavra = input('Digite uma palavra: ')
+
 print(f'Essa palavra tem {len(palavra)} letras!')
 
-
 while True:
+    
+    print(f'Tente advinhar a {cont}º letra')
     tentativa = input('Digite uma letra: ')
-   
+    if tentativa in palavra:
+        cont+=1
     if tentativa in palavra:
         tentativa_ok += tentativa
         print('Acertou!')
@@ -43,7 +28,13 @@ while True:
     if len(tentativa_nok) >= 6:
         print(f'Fim de jogo!! Você esgotou seu número de tentativas!')
         break
-    if tentativa_ok == palavra:
+    tentativa_ord = list(tentativa_ok)
+    tentativa_ord.sort()
+    palavra_ord = sorted(palavra)
+    if tentativa_ord == palavra_ord:
         print(f'Parabéns, você venceu!! A palavra é {palavra}')
         break
+
+print(f'As letras corretas foram: {separador.join(tentativa_ok)}')
+print(f'As letras incorretas foram: {separador.join(tentativa_nok)}')
 # %%
---
+++ ex_aula_DataTime.py
@@ -0,0 +1,41 @@
+#%%
+import datetime
+
+hoje = datetime.date.today()
+print(f'Hoje é dia {hoje.strftime("%d")}', end='')
+print(f"-{hoje.strftime('%m')}", end='')
+print(f"-{hoje.strftime('%y')}")
+# %%
+import time
+
+print(time.time())
+print(time.localtime(time.time()))
+#agora = datetime.datetime(ano, mês, dia, hora, minuto, segundo, microssegundo,timezone, fold)
+data = datetime.date(2022, 9, 14)
+hora = datetime.time(10,31,00)
+agora = datetime.datetime.now()
+print(data)
+print(hora)
+print(agora)
+
+# %%
+print('Usando informações temporais...')
+dias = ['segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sábado', 'domingo']
+agora = datetime.datetime.now()
+print(f'Agora são: {agora}')
+print('Extraindo os componentes do momento atual:')
+print(f'Dia: {agora.day}')
+print(f'Mês: {agora.month}')
+print(f'Ano: {agora.year}')
+print(f'Horas: {agora.hour}')
+print(f'Minutos: {agora.minute}')
+print(f'Segundos: {agora.second}')
+dia_da_semana = datetime.date.weekday(agora)
+print(f'Hoje é dia: {dias[dia_da_semana]}, o dia de numero {dia_da_semana + 2} da semana.')
+t1 = datetime.datetime(2022,9,6)
+t2 = datetime.datetime(2022,9,13)
+print(f'A diferença entre {t2} e {t1} é {t2 - t1}')
+print('Se você tentar somar duas variáveis datetime, obterá um erro:')
+print(t2 + t1)
+
+# %%
